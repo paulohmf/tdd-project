df41e0ab9e1f1863a4bf82849748518b8564e8d4
paulohmf <48098066+paulohmf@users.noreply.github.com>
2022-06-17 19:23:53 -0300

    Refatorando o Teste Funcional

--- functional_tests/test_simple_list_creation.py
+++ functional_tests/test_simple_list_creation.py
@@ -1,18 +1,10 @@
-from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-from selenium.common.exceptions import WebDriverException
-import time
+from .base import FunctionalTest
 
 MAX_WAIT = 5
 
-class NewVisitorTest(StaticLiveServerTestCase):
-
-    def setUp(self):
-        self.browser = webdriver.Firefox()
-
-    def tearDown(self):
-        self.browser.quit()
+class NewVisitorTest(FunctionalTest):
 
     def test_can_start_a_list_for_one_user(self): 
     
@@ -93,18 +85,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
 
         # Satisfeitos, ambos vão dormir
 
-    def wait_for_row_in_list_table(self, row_text):
-        start_time = time.time()
-        while True:  
-            try:
-                table = self.browser.find_element_by_id('id_list_table')  
-                rows = table.find_elements_by_tag_name('tr')
-                self.assertIn(row_text, [row.text for row in rows])
-                return  
-            except (AssertionError, WebDriverException) as e:  
-                if time.time() - start_time > MAX_WAIT:  
-                    raise e  
-                time.sleep(0.5)
+class LayoutAndStylingTest(FunctionalTest):
 
     def test_layout_and_styling(self):
         # Edith entra na home page
